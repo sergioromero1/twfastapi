27701fab7c2b23356d59ede5f1b717a57bf91d13
Sergio <sar9206@gmail.com>
2021-11-30 14:22:13 -0500

    validations_query

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 #FastApI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -20,9 +21,15 @@ class Person(BaseModel):
 def home():  # path operation function
     return {"Hello": "World"}
 
+#validaciones: Query parameters
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...) # no es lo ideal. lo ideal es que query parameter sea optional
+    ):
+    return {name:age}
 
 # Request and Response body
-
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     """Los ... en fastapi significa que son obligatorios"""
